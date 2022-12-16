8a1be4f94e72f97273258c63b3aee6c6207122c3
sehyuni <foxkh24100@hanyang.ac.kr>
2022-12-17 00:40:29 +0900

    admin, ornamentlist

--- wall/admin.py
+++ wall/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import    mixDeer, deer, RealWreath, Sock
+from .models import    mixDeer, deer, RealWreath, Sock, OrnamentList
 
 # Register your models here.
 admin.site.register(mixDeer)
 admin.site.register(deer)
 admin.site.register(RealWreath)
 admin.site.register(Sock)
+admin.site.register(OrnamentList)
